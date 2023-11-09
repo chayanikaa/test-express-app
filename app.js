571ab0e864d94906118641a586d5283f248ebee4
Chayanika Khatua <chayanika.khatua@gmail.com>
2023-11-09 10:59:12 +0400

    chore: add a factorial endpoint to test scaling

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
+const factorial = (n) => {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+};
+
 app.get('/', (req, res) => {
   res.send('Hello Express World! Test3');
+});
+
+app.get('/factorial', (req, res) => {
+  const number = Math.round(Math.random() * 100);
+  console.log(number)
+  res.send(`Factorial of ${number} is ${factorial(number)}`);
 });
 
 
